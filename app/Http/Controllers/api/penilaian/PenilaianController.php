02e5b6c8f7980a65fe6553092ff52936ae0d15d7
joshua-jojo <joshuasitompul2000@gmail.com>
2023-10-19 13:40:14 +0700

    update bug

--- app/Http/Controllers/api/penilaian/PenilaianController.php
+++ app/Http/Controllers/api/penilaian/PenilaianController.php
@@ -16,20 +16,37 @@ class PenilaianController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $penilaian = new Penilaian();
-        $penilaian->assessment_job_id = intval($request->assessment_job);
-        $penilaian->saran = $request->saran;
-        $penilaian->tanggapan = $request->tanggapan;
-        
-        for ($i=1; $i <= 27; $i++) { 
-            $penilaian["pertanyaan_$i"] = $request["pertanyaan_$i"];
-        }
+        $id_assessment_job = intval($request->assessment_job);
 
-        $penilaian->jumlah = intval($request->jumlah);
-        $penilaian->save();
-        return response()->json([
-            'success' => true,
-            'msg' => $penilaian
-        ]);
+        $penilaian_job = Penilaian::where('assessment_job_id',$id_assessment_job)->first();
+        if(empty($penilaian_job)){
+            $penilaian = new Penilaian();
+            $penilaian->assessment_job_id = $id_assessment_job;
+            $penilaian->saran = $request->saran;
+            $penilaian->tanggapan = $request->tanggapan;
+            
+            for ($i=1; $i <= 27; $i++) { 
+                $penilaian["pertanyaan_$i"] = $request["pertanyaan_$i"];
+            }
+    
+            $penilaian->jumlah = intval($request->jumlah);
+            $penilaian->save(); 
+            return response()->json([
+                'success' => true,
+                'msg' => $penilaian
+            ]);
+        }
+        else {
+            $penilaian_job->saran = $request->saran;
+            $penilaian_job->tanggapan = $request->tanggapan;
+            for ($i=1; $i <= 27; $i++) { 
+                $penilaian_job["pertanyaan_$i"] = $request["pertanyaan_$i"];
+                $penilaian_job->save();
+            }
+            return response()->json([
+                'success' => false,
+                'msg' => $penilaian_job
+            ]);
+        }
     }
 }
